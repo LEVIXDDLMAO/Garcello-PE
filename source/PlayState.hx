83e8fe7b455e0bfcedbe4c53d9f265092ca6549d
LEVIXDDLMAO <levi_xddd@outlook.com>
2022-10-24 04:36:50 +0000

    miss spelled

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1176,6 +1176,8 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
+				case 'headache' | 'nerves' |'release-gar' |'fading':
+					startDialogue(dialogueJson);
 
 				default:
 					startCountdown();
